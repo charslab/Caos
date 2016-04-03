ab97c037828875156334187a4ed7cba8e8943bc0
carloalberto <carloalberto@carloalbertos-Mac-Pro.local>
2016-04-03 13:03:21 +0200

    Added osx compile script

---
+++ compile_osx.sh
@@ -0,0 +1,6 @@
+scp *.c carloalberto@192.168.1.5:Documents/Caos/
+scp *.s carloalberto@192.168.1.5:Documents/Caos/
+scp *.ld carloalberto@192.168.1.5:Documents/Caos/
+scp *.sh carloalberto@192.168.1.5:Documents/Caos/
+ssh carloalberto@192.168.1.5 cd Documents/Caos ./compile.sh
+scp carloalberto@192.168.1.5:Documents/Caos/kernel.elf .
--- loader.s
+++ loader.s
@@ -13,7 +13,7 @@ global loader                   ; the entry symbol for ELF
     kernel_stack: 
         resb KERNEL_STACK_SIZE 
 
-    section .text:                  ; start of the text (code) section
+    section .text                  ; start of the text (code) section
     align 4                         ; the code must be 4 byte aligned
         dd MAGIC_NUMBER             ; write the magic number to the machine code,
         dd FLAGS                    ; the flags,
